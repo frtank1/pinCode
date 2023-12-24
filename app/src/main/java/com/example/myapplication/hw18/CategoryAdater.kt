893ae8eace43bac09f2b8e56a76dca2bf6cb2eac
frtank1 <frtank1@yande.ru>
2023-12-25 02:17:30 +0600

    Home work 18 viePager

---
+++ app/src/main/java/com/example/myapplication/hw18/CategoryAdater.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.hw18
+
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class CategoryAdater(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> MainFragment()
+            1 -> StatisticFragment()
+            2 -> AnotherFragment()
+            else -> throw IllegalArgumentException("Invalid position: $position")
+        }
+    }
+
+
+
+}
